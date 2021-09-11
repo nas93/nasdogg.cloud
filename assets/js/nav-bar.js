02ccd88be3e478edf2979ec89082fb0cb5de2269
snobdogg <jonas.birkeli@hotmail.no>
2021-09-11 21:11:08 +0200

    Structural Commit Bypass Fix

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,18 +1,11 @@
 var navBar = document.getElementById("nav-links-mobile");
-    function showMenu() {
-        navBar.style.right = "0";
-    }
-    function hideMenu() {
-        navBar.style.right = "-200px";
-    }
 
-const parallax = document.getElementById("main-page");
+function showMenu() 
+{
+    navBar.style.right = "0";
+}
 
-window.addEventListener("scroll", function(){
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * 0.7 + "px";
-    
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0)
-})
-
+function hideMenu() 
+{
+    navBar.style.right = "-200px";
+}
