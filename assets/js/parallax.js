1a82795d1372e28fb1a1b6aa4d759c479cf79329
snobdogg <jonas.birkeli@hotmail.no>
2021-09-12 10:08:19 +0200

    Parallax Implementation Progress

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,9 +1,12 @@
 const parallax = document.getElementById("main-page");
+const cloud_parallax = document.getElementById("cloud-parallax");
+
 
 window.addEventListener("scroll", function()
 {
 
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    cloud_parallax.style.right = offset * 1 + "%";
 
 });
