137194b85fd60d1fd9cb993e6b30fc8ec45327f5
snobdogg <jonas.birkeli@hotmail.no>
2021-09-24 16:42:10 +0200

    Update parallax.js

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -5,6 +5,7 @@ const parallaxC = document.getElementById("contact");
 window.addEventListener("scroll", function()
 {
     let offset = window.pageYOffset;
+    console.log(offset);
     parallaxMain.style.backgroundPositionY = offset * 0.7 + "px";
-    parallaxC.style.backgroundPositionY = offset * 0.7 + 800+ "px";
+    parallaxC.style.backgroundPositionY = offset * 0.7 + 2720 + "px";
 });
