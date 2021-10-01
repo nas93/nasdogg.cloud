969315ff90904384eb71977e8a34b3ec51a98c7f
snobdogg <jonas.birkeli@hotmail.no>
2021-10-01 20:13:25 +0200

    Background and Cleaning Commit

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,5 +1,4 @@
 const parallaxMain = document.getElementById("main-page");
-const parallaxC = document.getElementById("contact");
 
 
 window.addEventListener("scroll", function()
@@ -7,5 +6,4 @@ window.addEventListener("scroll", function()
     let offset = window.pageYOffset;
     console.log(offset);
     parallaxMain.style.backgroundPositionY = offset * 0.7 + "px";
-    parallaxC.style.backgroundPositionY = offset * 0.7 + 2720 + "px";
 });
