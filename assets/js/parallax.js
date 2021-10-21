e20d682afdfe50f181c96582fec8cdd14d724458
snobdogg <jonas.birkeli@hotmail.no>
2021-10-21 16:14:20 +0200

    Revamp Commit

--- assets/js/parallax.js
+++
@@ -1,8 +0,0 @@
-const parallaxMain = document.getElementById("main-page");
-
-
-window.addEventListener("scroll", function()
-{
-    let offset = window.pageYOffset;
-    parallaxMain.style.backgroundPositionY = offset * 0.7 + "px";
-});
